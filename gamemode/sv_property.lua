1e93f0bc0e66b7f3448c80c60459dbcae50f3464
charozoid <directfromquebec@gmail.com>
2017-05-07 12:04:05 -0400

    fixed properties

--- gamemode/sv_property.lua
+++ gamemode/sv_property.lua
@@ -4,22 +4,22 @@ function Player:RentProperty(id)
 	if not PROPERTY_DB[id] || not self:IsValid() then return end
 	for _, v in pairs(PROPERTY_DB[id].Doors) do
 		for _, u in pairs(ents.FindInSphere(v.Pos, 5)) do
-			if v.Model == u:GetModel() && u.Owner == NULL then
-				u:Setowner(self)
-			end
+			if not v.Model == u:GetModel() || not u.Owner == NULL then return end
+			u:Setowner(self)
 		end
 	end
+	self:RemoveMoney(PROPERTY_DB[id].Price)
 end
 
 function Player:VacantProperty(id)
 	if not PROPERTY_DB[id] || not self:IsValid() then return end
 	for _, v in pairs(PROPERTY_DB[id].Doors) do
 		for _, u in pairs(ents.FindInSphere(v.Pos, 5)) do
-			if v.Model == u:GetModel() then
-				u:Setowner(NULL)
-			end
+			if not v.Model == u:GetModel() || not v.Owner == self then return end
+			u:Setowner(NULL)
 		end
 	end
+	self:AddMoney(PROPERTY_DB[id].Price/2)
 end
 
 -----------------
